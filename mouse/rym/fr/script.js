208abd0d1301ad0f597645bd02f968fcb35562c8
Harshu082005 <165150395+Harshu082005@users.noreply.github.com>
2024-08-04 09:45:06 +0530

    Delete mouse directory

--- mouse/rym/fr/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("me").style.color = "blue";
-
-document.getElementById("bun").addEventListener("click", function() {
-    document.getElementById("me").style.fontFamily = "Cursive";
-});
-
-document.getElementById("bun2").addEventListener("click", function() {
-    document.getElementById("mine").style.color = "red";
-});
